1e7eff2425cd91b5b662764dbf1adf599a02e060
quangloc <locpq0311@gmail.com>
2019-05-11 13:53:27 +0700

    update sketch

--- 2048/public/javascripts/sketch.js
+++ 2048/public/javascripts/sketch.js
@@ -13,52 +13,6 @@ function setup() {
     updateCanvas();
 }
 
-function addNumber() {
-    let options = [];
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (grid[i][j] === 0) {
-                options.push({
-                    x: i,
-                    y: j
-                });
-            }
-        }
-    }
-    if (options.length > 0){
-        let spot = random(options);
-        let r = random(1);
-        grid[spot.x][spot.y] = r > 0.5 ? 2 : 4;
-    }
-}
-
-
-function copyGrid(grid) {
-    let extra = blankGrid();
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            extra[i][j] = grid[i][j];
-        }
-    }
-    return extra;
-}
-
-function flipGrid(grid) {
-    for (let i = 0; i < 4; i++) {
-        grid[i].reverse();
-    }
-    return grid;
-}
-
-function rotateGrid(grid) {
-    let newGrid = blankGrid();
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            newGrid[i][j] = grid[j][i];
-        }
-    }
-    return newGrid;
-}
 
 // one "move"
 function keyPressed() {
@@ -131,28 +85,6 @@ function updateCanvas() {
     document.getElementById('score').innerHTML = score;
 }
 
-//making new array
-function slide(row) {
-    let arr = row.filter(val => val);
-    let missing = 4 - arr.length;
-    let zeros = Array(missing).fill(0);
-    arr = zeros.concat(arr);
-    return arr;
-}
-
-// //operating on array itself
-function combine(row) {
-    for (let i = 3; i >= 1; i--) {
-        let a = row[i];
-        let b = row[i - 1];
-        if (a == b) {
-            row[i] = a + b;
-            score += row[i];
-            row[i - 1] = 0;
-        }
-    }
-    return row;
-}
 
 function drawGrid() {
     let w = 100;
