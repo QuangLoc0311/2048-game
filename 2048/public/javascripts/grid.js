c20c42cf624e5388bce9647f87f3a94bf44cfb1f
quangloc <locpq0311@gmail.com>
2019-05-11 11:44:48 +0700

    update grid.js

---
+++ 2048/public/javascripts/grid.js
@@ -0,0 +1,20 @@
+function blankGrid() {
+    return [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ];
+}
+
+
+function compare(a, b) {
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (a[i][j] !== b[i][j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
